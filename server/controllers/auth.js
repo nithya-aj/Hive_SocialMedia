49c7f2060977610c51be0603d5f23f704ebe0481
Nithya AJ <aj7nithya@gmail.com>
2023-07-13 22:51:12 +0530

    user registration

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,15 +1,25 @@
 import { db } from "../connect.js"
+import bcrypt from 'bcryptjs'
 
 // user registration
 export const register = (req, res) => {
     // check user if exists
-    const q = "SELECT FROM users WHERE userName = ?"
-    db.query(q, [req.body.userName], (err, data)=>{
-        if(err) return res.status(500).json(err)
-        if(data.length) return res.status(409).json("User already exists!")
+    const q = "SELECT * FROM users WHERE userName = ?"
+    db.query(q, [req.body.userName], (err, data) => {
+        if (err) return res.status(500).json(err)
+        if (data.length) return res.status(409).json("User already exists!")
+        // create new user
+        // hash password
+        const salt = bcrypt.genSaltSync(10)
+        const hashedPassword = bcrypt.hashSync(req.body.password, salt)
+
+        const q = "INSERT INTO users (`userName`,`email`,`password`,`firstName`,`lastName` ) VALUE (?)"
+        const values = [req.body.userName, req.body.email, hashedPassword, req.body.firstName, req.body.lastName]
+        db.query(q, [values], (err, data) => {
+            if (err) return res.status(500).json(err)
+            return res.status(200).json("User has been created!")
+        })
     })
-    // create new user
-    // hash password
 }
 
 // user login
