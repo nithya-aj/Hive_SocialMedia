ee9fa9b64e92973289cda17569fac82b633bcb4f
Nithya AJ <aj7nithya@gmail.com>
2023-08-13 22:49:23 +0530

    updated input

--- client/src/components/widget/InputBar.jsx
+++ client/src/components/widget/InputBar.jsx
@@ -7,18 +7,19 @@ import { Box } from '@mui/material';
 const InputBar = () => {
 
     const theme = useTheme()
-    const textbg = theme.palette.neutral.main
-    const main = theme.palette.background.main
+    const main = theme.palette.neutral.main
+    const darkbg = theme.palette.background.darkbg
+    const alt = theme.palette.background.alt
 
     return (
         <Box sx={{ width: '100%' }}>
             <Paper
                 component="form"
-                sx={{ display: 'flex', alignItems: 'center', height: "2.5rem", backgroundColor: main, boxShadow: 'none', borderRadius: { sm: '10px', xs: '44px' } }}
+                sx={{ display: 'flex', alignItems: 'center', height: "2.5rem", backgroundColor: { xs: darkbg, sm: alt }, boxShadow: 'none', borderRadius: { sm: '10px', xs: '44px' } }}
             >
                 <InputBase
-                    sx={{ ml: { sm: 1, xs: 2 }, flex: 1, color: textbg }}
-                    placeholder='What do you want to share today?'
+                    sx={{ ml: { sm: 1, xs: 2 }, flex: 1, color: main }}
+                    placeholder='What do you want to share today?' 
                     inputProps={{ 'aria-label': 'search google maps' }}
                 />
             </Paper>
