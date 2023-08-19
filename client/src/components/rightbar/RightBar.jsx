da30544b365d74369031ba1679dfa8cd14209900
Nithya AJ <aj7nithya@gmail.com>
2023-08-19 21:11:13 +0530

    right bar updated

--- client/src/components/rightbar/RightBar.jsx
+++ client/src/components/rightbar/RightBar.jsx
@@ -1,6 +1,5 @@
-import { Avatar, Box, Divider, IconButton, Typography } from '@mui/material'
+import { Avatar, Box, Divider, Typography } from '@mui/material'
 import React from 'react'
-import MoreVertIcon from '@mui/icons-material/MoreVert';
 import FlexCenter from 'components/widget/FlexCenter';
 import SearchBar from 'components/widget/SearchBar';
 import { useTheme } from '@emotion/react';
@@ -12,10 +11,11 @@ function RightBar() {
     const main = theme.palette.background.main
     const alt = theme.palette.background.alt
     const dark = theme.palette.neutral.dark
-    const mediumMain = theme.palette.neutral.mediumMain
+    const mediumMain = theme.palette.neutral.mediumMain 
     const medium = theme.palette.neutral.medium
-    const light = theme.palette.neutral.light 
+    const light = theme.palette.neutral.light
     const textMain = theme.palette.neutral.main
+    const fontSm = theme.palette.neutral.fontSm
 
     return (
         <Box sx={{ display: 'flex', gap: '0.8rem', flexDirection: 'column', height: '100%', pb: '0.5rem', px: '0.8rem', p: '1rem 0.8rem 0.5rem 0.8rem' }}>
@@ -36,64 +36,64 @@ function RightBar() {
                 </FlexCenter>
             </Box>
             <Box sx={{ overflow: 'auto', display: 'flex', flexDirection: 'column' }}>
-                <Box sx={{ display: 'flex', alignItems: 'center', gap: '1rem', p: '0.8rem 0.4rem', ":hover": { backgroundColor: alt, borderRadius: '10px' } }}>
-                    <Avatar aria-label="avatar" sx={{ height: '2.3rem', width: '2.3rem', backgroundColor: light, color: textMain, }}>
-                        R
-                    </Avatar>
-                    <Box sx={{
-                        overflow: 'hidden', display: 'inline-block',
-                        whiteSpace: 'nowrap',
-                        textOverflow: 'ellipsis',
-                        color: medium
-                    }}>
-                        <Typography sx={{ fontSize: '0.75rem', color: dark }}>Alex</Typography>
-                        <Typography variant="caption" noWrap sx={{ color: medium }} >
-                            Can you please share your latest work on react, I want to check something...
-                        </Typography>
+                <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+                    <Box sx={{ display: 'flex', alignItems: 'center', gap: '1rem', p: '0.8rem 0.4rem', width: '84%', ":hover": { backgroundColor: alt, borderRadius: '10px' } }}>
+                        <Avatar aria-label="avatar" sx={{ height: '2.3rem', width: '2.3rem', backgroundColor: light, color: textMain, }}>
+                            R
+                        </Avatar>
+                        <Box sx={{
+                            overflow: 'hidden', display: 'inline-block',
+                            whiteSpace: 'nowrap',
+                            textOverflow: 'ellipsis',
+                            color: medium
+                        }}>
+                            <Typography sx={{ fontSize: '0.75rem', color: dark }}>Alex</Typography>
+                            <Typography variant="caption" noWrap sx={{ color: medium }} >
+                                Can you please share your latest work on react, I want to check something...
+                            </Typography>
+                        </Box>
                     </Box>
-                    <IconButton aria-label="settings">
-                        <MoreVertIcon sx={{ color: medium }} />
-                    </IconButton>
+                    <Typography variant='subtitle2' sx={{ fontSize: '8px', color: fontSm }}>2 min</Typography>
                 </Box>
 
-                <Box sx={{ display: 'flex', alignItems: 'center', gap: '1rem', p: '0.8rem 0.4rem', ":hover": { backgroundColor: alt, borderRadius: '10px' } }}>
-                    <Avatar aria-label="avatar" sx={{ height: '2.3rem', width: '2.3rem', backgroundColor: light, color: textMain, }}>
-                        R
-                    </Avatar>
-                    <Box sx={{
-                        overflow: 'hidden', display: 'inline-block',
-                        whiteSpace: 'nowrap',
-                        textOverflow: 'ellipsis',
-                        color: medium
-                    }}>
-                        <Typography sx={{ fontSize: '0.75rem', color: dark }}>Alex</Typography>
-                        <Typography variant="caption" noWrap sx={{ color: medium }} >
-                            Can you please share your latest work on react, I want to check something...
-                        </Typography>
+                <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+                    <Box sx={{ display: 'flex', alignItems: 'center', gap: '1rem', p: '0.8rem 0.4rem', width: '84%', ":hover": { backgroundColor: alt, borderRadius: '10px' } }}>
+                        <Avatar aria-label="avatar" sx={{ height: '2.3rem', width: '2.3rem', backgroundColor: light, color: textMain, }}>
+                            R
+                        </Avatar>
+                        <Box sx={{
+                            overflow: 'hidden', display: 'inline-block',
+                            whiteSpace: 'nowrap',
+                            textOverflow: 'ellipsis',
+                            color: medium
+                        }}>
+                            <Typography sx={{ fontSize: '0.75rem', color: dark }}>Alex</Typography>
+                            <Typography variant="caption" noWrap sx={{ color: medium }} >
+                                Can you please share your latest work on react, I want to check something...
+                            </Typography>
+                        </Box>
                     </Box>
-                    <IconButton aria-label="settings">
-                        <MoreVertIcon sx={{ color: medium }} />
-                    </IconButton>
+                    <Typography variant='subtitle2' sx={{ fontSize: '8px', color: fontSm }}>2 min</Typography>
                 </Box>
 
-                <Box sx={{ display: 'flex', alignItems: 'center', gap: '1rem', p: '0.8rem 0.4rem', ":hover": { backgroundColor: alt, borderRadius: '10px' } }}>
-                    <Avatar aria-label="avatar" sx={{ height: '2.3rem', width: '2.3rem', backgroundColor: light, color: textMain, }}>
-                        R
-                    </Avatar>
-                    <Box sx={{
-                        overflow: 'hidden', display: 'inline-block',
-                        whiteSpace: 'nowrap',
-                        textOverflow: 'ellipsis',
-                        color: medium
-                    }}>
-                        <Typography sx={{ fontSize: '0.75rem', color: dark }}>Alex</Typography>
-                        <Typography variant="caption" noWrap sx={{ color: medium }} >
-                            Can you please share your latest work on react, I want to check something...
-                        </Typography>
+                <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+                    <Box sx={{ display: 'flex', alignItems: 'center', gap: '1rem', p: '0.8rem 0.4rem', width: '84%', ":hover": { backgroundColor: alt, borderRadius: '10px' } }}>
+                        <Avatar aria-label="avatar" sx={{ height: '2.3rem', width: '2.3rem', backgroundColor: light, color: textMain, }}>
+                            R
+                        </Avatar>
+                        <Box sx={{
+                            overflow: 'hidden', display: 'inline-block',
+                            whiteSpace: 'nowrap',
+                            textOverflow: 'ellipsis',
+                            color: medium
+                        }}>
+                            <Typography sx={{ fontSize: '0.75rem', color: dark }}>Alex</Typography>
+                            <Typography variant="caption" noWrap sx={{ color: medium }} >
+                                Can you please share your latest work on react, I want to check something... 
+                            </Typography>
+                        </Box>
                     </Box>
-                    <IconButton aria-label="settings">
-                        <MoreVertIcon sx={{ color: medium }} />
-                    </IconButton>
+                    <Typography variant='subtitle2' sx={{ fontSize: '8px', color: fontSm }}>2 min</Typography>
                 </Box>
             </Box>
         </Box>
