f0f8f851624e48e34472376846794447b16ddfa8
Nithya AJ <aj7nithya@gmail.com>
2023-08-20 01:00:11 +0530

    working on profile right section

--- client/src/components/Profile/ProfileSectionRight.jsx
+++ client/src/components/Profile/ProfileSectionRight.jsx
@@ -1,9 +1,14 @@
 import { Box } from '@mui/material'
+import FriendsReq from 'components/home/PostRight/FriendsReq'
+import ProfileCard from 'components/widget/ProfileCard'
 import React from 'react'
 
 const ProfileSectionRight = () => {
   return (
-    <Box sx={{ backgroundColor: 'red' }}>rigt</Box>
+    <Box sx={{ display: 'flex', flexDirection: 'column', gap: '1rem' }}> 
+      <ProfileCard />
+      <FriendsReq />
+    </Box>
   )
 }
 
