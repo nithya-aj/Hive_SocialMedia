bf2e729065b09756790f0b08d4fb16dceb58ae34
Nithya AJ <aj7nithya@gmail.com>
2023-08-24 00:16:22 +0530

    updated profile card

--- client/src/components/widget/ProfileCard.jsx
+++ client/src/components/widget/ProfileCard.jsx
@@ -1,24 +1,32 @@
 import { useTheme } from '@emotion/react';
-import { Box, Button, Card, CardActions, CardMedia, Typography } from '@mui/material'
-import UserAvatar from 'components/widget/UserAvatar'
+import { Box, Card, IconButton, Typography } from '@mui/material'
 import React from 'react'
-import { BsFillArrowRightCircleFill } from "react-icons/bs";
 import { MdEmail } from "react-icons/md";
 import { HiAtSymbol } from "react-icons/hi";
 import { HiBuildingOffice } from "react-icons/hi2";
+import { MdCake, MdEdit } from "react-icons/md";
+import { FaUserTag } from "react-icons/fa";
+import FlexCenter from './FlexCenter';
+
 
 const ProfileCard = () => {
 
-    const theme = useTheme() 
+    const theme = useTheme()
     const darkbg = theme.palette.background.darkbg
     const main = theme.palette.neutral.main
     const fontSm = theme.palette.neutral.fontSm
 
     return (
         <Card sx={{ borderRadius: '10px', backgroundColor: darkbg, boxShadow: 'none' }}>
-            <Box sx={{ height: '6rem', bgcolor: fontSm }}>
-
-            </Box>
+            <FlexCenter sx={{ height: '4rem', bgcolor: fontSm, position: 'relative' }}>
+                <Typography variant='h4'>Irin Rose</Typography>
+                <Box sx={{
+                    height: "2.1rem", width: '2.1rem', backgroundColor: fontSm, position: 'absolute', top: '3.2rem', left: '15rem',
+                    display: 'flex', alignItems: 'center', justifyContent: 'center', borderRadius: '50%', border: `2px solid ${darkbg}`
+                }}>
+                    <IconButton ><MdEdit size={'1rem'} style={{ color: darkbg }} /></IconButton>
+                </Box>
+            </FlexCenter>
             <Box sx={{ display: 'flex', flexDirection: 'column', gap: 1, p: '1rem', color: main }}>
                 <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
                     <HiAtSymbol />
@@ -30,6 +38,14 @@ const ProfileCard = () => {
                 </Box>
                 <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
                     <HiBuildingOffice />
+                    <Typography sx={{ width: '70%' }}>Ernakulam</Typography>
+                </Box>
+                <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+                    <MdCake />
+                    <Typography sx={{ width: '70%' }}>Fex 21 2002</Typography>
+                </Box>
+                <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+                    <FaUserTag />
                     <Typography sx={{ width: '70%' }}>Ernakulam</Typography>
                 </Box>
             </Box>
