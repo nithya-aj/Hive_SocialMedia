71edf7ff340eb0d28f0f90672ffc9e979320bdf7
Nithya AJ <aj7nithya@gmail.com>
2023-09-08 23:16:30 +0530

    getPost in postControllers

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -2,7 +2,12 @@ import User from '../models/User.js'
 import Post from '../models/Post.js'
 
 export const getPost = async (req, res) => {
-
+    try {
+        const post = await Post.findById(req.params.postId);
+        return res.status(200).json(post);
+    } catch (error) {
+        return res.status(500).json(error.message);
+    }
 }
 
 export const getUserPosts = async (req, res) => {
