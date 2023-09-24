e4fd16a0e8bbd9dcfc383106a01e99e91a802ca5
Nithya AJ <aj7nithya@gmail.com>
2023-09-25 00:54:07 +0530

    complete profile modal

--- client/src/components/home/PostRight/UpdateProfile.jsx
+++ client/src/components/home/PostRight/UpdateProfile.jsx
@@ -1,7 +1,8 @@
 import { useTheme } from '@emotion/react';
 import { Button, Card, CardActions, CardMedia, Typography } from '@mui/material'
+import CompleProfile from 'components/Modals/CompleProfile';
 import UserAvatar from 'components/widget/UserAvatar'
-import React from 'react'
+import React, { useState } from 'react'
 import { BsFillArrowRightCircleFill } from "react-icons/bs";
 
 const UpdateProfile = () => {
@@ -13,17 +14,25 @@ const UpdateProfile = () => {
     const purple = theme.palette.neutral.purple
     const mediumpurple = theme.palette.neutral.mediumpurple
 
+    const [open, setOpen] = useState(false)
+    const handleOpen = () => setOpen(true)
+    const handleClose = () => setOpen(false)
+
     return (
         <Card sx={{ borderRadius: '10px', backgroundColor: darkbg, boxShadow: 'none' }}>
             <CardMedia sx={{ height: '8rem', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', }}>
-                <UserAvatar />  
+                <UserAvatar />
                 <Typography sx={{ fontSize: '19px', fontWeight: 300, mt: '0.5rem', color: main }}>Update your profile</Typography>
                 <Typography variant='caption' sx={{ color: medium }}>Let's complete your profile!</Typography>
             </CardMedia>
             <CardActions sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', pb: '1.5rem' }}>
-                <Button sx={{ display: { xs: 'none', sm: 'flex' }, textTransform: 'none', width: '90%', height: '2.5rem', backgroundColor: purple, fontSize: '15px', color: darkbg, ":hover": { backgroundColor: mediumpurple } }} variant="contained" endIcon={<BsFillArrowRightCircleFill />}>
+                <Button onClick={handleOpen} sx={{ display: { xs: 'none', sm: 'flex' }, textTransform: 'none', width: '90%', height: '2.5rem', backgroundColor: purple, fontSize: '15px', color: darkbg, ":hover": { backgroundColor: mediumpurple } }} variant="contained" endIcon={<BsFillArrowRightCircleFill />}>
                     Update Now
                 </Button>
+                {
+                    open &&
+                    <CompleProfile open={open} handleOpen={handleOpen} handleClose={handleClose} />
+                }
             </CardActions>
         </Card>
     )
