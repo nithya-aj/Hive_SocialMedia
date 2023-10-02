bf8f16a7fcb23a5368ed1055efbd472148d221ce
Nithya AJ <aj7nithya@gmail.com>
2023-10-02 11:24:12 +0530

    working on comments part

--- client/src/components/home/Post.jsx
+++ client/src/components/home/Post.jsx
@@ -10,7 +10,7 @@ import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import UserAvatar from '../widget/UserAvatar';
-import { Avatar, Box, FormControl, Grid, Input, InputAdornment, List, ListItemText, Menu, MenuItem } from '@mui/material';
+import { Box, FormControl, Input, InputAdornment, List, Menu, MenuItem } from '@mui/material';
 import { MdOutlineInsertComment, MdTurnedInNot, MdOutlineFavoriteBorder, MdDeleteOutline } from "react-icons/md";
 import { BiHide, BiSend } from "react-icons/bi";
 import { useTheme } from '@mui/material/styles';
@@ -23,6 +23,7 @@ import { useState } from 'react';
 import { useEffect } from 'react';
 import api from 'api';
 import { addComment, getComments } from 'redux/commentSlice';
+import Comment from 'components/Comments/Comment';
 
 const ExpandMore = styled((props) => {
     const { expand, ...other } = props;
@@ -279,31 +280,8 @@ export default function Post({ post }) {
                             '& ul': { padding: 0 },
                             padding: '0rem'
                         }}>
-                        {comments?.length > 0 ? comments.map((comment, id) => (
-                            <Grid container key={comment._id}>
-                                <Grid item xs={2} lg={1.3} md={1.5} >
-                                    <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', mt: '5px' }}>
-                                        <Avatar alt="Remy Sharp" src="https://source.unsplash.com/featured/300x198" sx={{ height: '2.3rem', width: '2.3rem' }} />
-                                    </Box>
-                                </Grid>
-                                <Grid item xs={10} lg={10.7} md={10.5}>
-                                    <ListItemText sx={{ display: 'flex', flexDirection: 'column' }}>
-                                        <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
-                                            <Typography sx={{ color: medium }}>
-                                                Ali Connors
-                                            </Typography>
-                                            <Typography sx={{ color: medium, fontSize: '9px' }} variant='caption'>
-                                                3 days ago
-                                            </Typography>
-                                        </Box>
-                                        <Box sx={{ mt: '2px' }}>
-                                            <Typography variant='body2' sx={{ color: medium, fontSize: '12px' }}>
-                                                {comment.text}
-                                            </Typography>
-                                        </Box>
-                                    </ListItemText>
-                                </Grid>
-                            </Grid>
+                        {comments?.length > 0 ? comments.map((comment) => (
+                            <Comment comment={comment} key={comment._id} />
                         )) : (
                             <Typography>No comments yet!</Typography>
                         )}
