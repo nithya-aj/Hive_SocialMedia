caf011c79cb7f3c2703c9f4175bd9c96b7ee8053
Nithya AJ <aj7nithya@gmail.com>
2023-10-26 22:20:47 +0530

    added packages morgan and helmet

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
 import path from "path";
+import morgan from "morgan";
+import helmet from "helmet";
 import bodyParser from "body-parser";
 import router from './routes/index.js'
 import dbConnection from './dbConfig/index.js';
@@ -12,18 +14,17 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, "views/build")));
 
-const PORT = process.env.PORT || 8800;
-
 // db connect
 dbConnection()
 
 app.use('/images', express.static('public/images'))
+app.use(helmet());
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
-
+app.use(morgan("dev"));
 app.use(router)
 
 app.listen(process.env.PORT, () => console.log(`Server connected succesfully🚀`))
